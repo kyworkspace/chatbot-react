11e60878d3c85f72552ce92e390d96dda28198c0
unknown <ky91zzang@naver.com>
2021-02-03 15:18:36 +0900

    first

--- server/routes/dialogflow.js
+++ server/routes/dialogflow.js
@@ -2,14 +2,4 @@ const express = require('express');
 const router = express.Router();
 const structjson = require('./structjson.js');
 
-// Text Query
-router.post('/textQuery', (req,res)=>{
-
-    // 입력정보 전함
-
-})
-
-
-// Event Query
-
 module.exports = router;

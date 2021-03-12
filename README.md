85f06b0338347b80f6d909517d533097ce47d65c
ParkKiYoung <ky91zzang@naver.com>
2021-03-12 16:24:10 +0900

    restart

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# mern-chatbot-app
 # chatbot-app
+## 챗봇 만들기
+### 2021-02-03
+- dialogflow에서 회원가입
+- 구글 API에서 서비스키를 JSON 형태로 받음
+- config 안 dev 파일 안에 코드 입력
+
+
--- server/routes/dialogflow.js
+++ server/routes/dialogflow.js
@@ -1,5 +1,65 @@
 const express = require('express');
 const router = express.Router();
 const structjson = require('./structjson.js');
+const dialogflow = require('dialogflow');
+const uuid = require('uuid');
+const config = require("../config/keys");
+
+
+const projectId = 'test-chat-bot-app-303705';
+const sessionId = 'bot-session'
+const languageCode = 'en-US';
+
+// Create a new session
+const sessionClient = new dialogflow.SessionsClient();
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+console.log('sessionPath',sessionPath)
+
+router.post('/textQuery',async (req,res)=>{
+    console.log(req.body)
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: req.body.text,
+                languageCode: languageCode,
+            },
+        },
+    };
+    
+    // Send request and log result
+    const responses = await sessionClient.detectIntent(request);
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+
+    //프론트에 정보 리턴
+    res.send(result);
+})
+
+//evetQuery
+router.post('/eventQuery',async (req,res)=>{
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            event: {
+                name: req.body.event,
+                languageCode: languageCode,
+            },
+        },
+    };
+    
+    // Send request and log result
+    const responses = await sessionClient.detectIntent(request);
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+
+    //프론트에 정보 리턴
+    res.send(result);
+})
 
 module.exports = router;
